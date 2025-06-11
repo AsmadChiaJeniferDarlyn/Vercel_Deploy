d992915bfc5747452e9b9ad73bc9e20ce6bc368a
Darlyn A.C. <145638485+AsmadChiaJeniferDarlyn@users.noreply.github.com>
2025-06-11 10:19:36 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -1,11 +1,9 @@
 <?php
-/*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
-*****/
-
 $nacional = [
-"titulo" => "Titulo 2",
-"autor" => "Autor 2",
-"resumen" => "Resumen 2",
+  "titulo" => "Reforma constitucional avanza en el Congreso",
+  "autor" => "Carlos Torres - Redacción Política",
+  "resumen" => "El Senado aprobó en primera instancia la reforma que modifica el sistema de pensiones. Se esperan debates intensos en las próximas semanas.",
+  "imagen" => "https://cdn.pixabay.com/photo/2016/11/18/14/34/congress-1837046_1280.jpg"
 ];
 ?>
+
