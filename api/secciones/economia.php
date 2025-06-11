06e9320020da7868f519669a1d02f58173ee1902
Darlyn A.C. <145638485+AsmadChiaJeniferDarlyn@users.noreply.github.com>
2025-06-11 10:19:59 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -1,11 +1,8 @@
 <?php
-/*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
-*****/
-
 $economia = [
-"titulo" => "Titulo 3",
-"autor" => "Autor 3",
-"resumen" => "Resumen 3",
+  "titulo" => "Inflación en América Latina cae por tercer mes consecutivo",
+  "autor" => "Mariana Díaz - Economía Global",
+  "resumen" => "Los datos de mayo muestran una tendencia a la baja en los precios, impulsada por la caída del petróleo y ajustes en políticas monetarias.",
+  "imagen" => "https://cdn.pixabay.com/photo/2015/09/04/23/28/stock-922245_1280.jpg"
 ];
 ?>
