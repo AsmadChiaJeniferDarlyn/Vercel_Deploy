c5d455c003b821b2a6217d50d72f71d390c22751
Darlyn A.C. <145638485+AsmadChiaJeniferDarlyn@users.noreply.github.com>
2025-06-11 10:23:10 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -3,7 +3,7 @@ $internacional = [
   "titulo" => "Tensiones en el Mar de China Meridional escalan",
   "autor" => "Laura Méndez - Corresponsal Asia",
   "resumen" => "China y Filipinas protagonizan un nuevo incidente naval en aguas disputadas. La comunidad internacional pide moderación ante el riesgo de una confrontación mayor.",
-  "imagen" => "https://cdn.pixabay.com/photo/2020/04/21/18/19/sea-5072513_1280.jpg"
+  "imagen" => "https://pixabay.com/es/photos/tren-transporte-carriles-9596810/"
 ];
 ?>
 
