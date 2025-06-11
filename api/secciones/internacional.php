7ca92e2f0018e700bdf897f626054af21a66ce97
Darlyn A.C. <145638485+AsmadChiaJeniferDarlyn@users.noreply.github.com>
2025-06-11 10:12:38 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Titulo 1",
+"autor" => "Autor 1",
+"resumen" => "Resumen 1",
 ];
 ?>
