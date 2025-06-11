69b368c68b0612fdfb13d4ddd0b0efb5d0a287cf
Darlyn A.C. <145638485+AsmadChiaJeniferDarlyn@users.noreply.github.com>
2025-06-11 10:23:31 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -3,7 +3,7 @@ $nacional = [
   "titulo" => "Reforma constitucional avanza en el Congreso",
   "autor" => "Carlos Torres - Redacción Política",
   "resumen" => "El Senado aprobó en primera instancia la reforma que modifica el sistema de pensiones. Se esperan debates intensos en las próximas semanas.",
-  "imagen" => "https://cdn.pixabay.com/photo/2016/11/18/14/34/congress-1837046_1280.jpg"
+  "imagen" => "https://pixabay.com/es/photos/tren-transporte-carriles-9596810/"
 ];
 ?>
 
