d421ee0cf295c7017c6e2cdf1fe5aa61670e8dc5
Darlyn A.C. <145638485+AsmadChiaJeniferDarlyn@users.noreply.github.com>
2025-06-11 10:19:18 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -1,11 +1,9 @@
 <?php
-/*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
-*****/
-
 $internacional = [
-"titulo" => "Titulo 1",
-"autor" => "Autor 1",
-"resumen" => "Resumen 1",
+  "titulo" => "Tensiones en el Mar de China Meridional escalan",
+  "autor" => "Laura Méndez - Corresponsal Asia",
+  "resumen" => "China y Filipinas protagonizan un nuevo incidente naval en aguas disputadas. La comunidad internacional pide moderación ante el riesgo de una confrontación mayor.",
+  "imagen" => "https://cdn.pixabay.com/photo/2020/04/21/18/19/sea-5072513_1280.jpg"
 ];
 ?>
+
