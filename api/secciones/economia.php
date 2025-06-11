5fc2373b72fbfa75524861f556469550d0bf73a0
Darlyn A.C. <145638485+AsmadChiaJeniferDarlyn@users.noreply.github.com>
2025-06-11 10:13:34 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Titulo 3",
+"autor" => "Autor 3",
+"resumen" => "Resumen 3",
 ];
 ?>
